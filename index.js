7f56535b1cb7f6f71da54f08aeac9e727ab0da5c
Krishna-77777 <122208388+Krishna-77777@users.noreply.github.com>
2024-09-04 00:09:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const yesBtn = document.getElementById('btn-yes');
+const noBtn = document.getElementById('btn-no');
+
+const deviceWidth = window.innerWidth;
+const deviceHeight = window.innerHeight;
+
+yesBtn.addEventListener('touchstart', yes);
+yesBtn.addEventListener('click', yes);
+
+noBtn.addEventListener('touchstart', no);
+noBtn.addEventListener('click', no);
+
+const buttonRect = noBtn.getBoundingClientRect();
+
+const buttonX = buttonRect.left;
+const buttonY = buttonRect.top;
+
+
+function no() {
+    const buttonWidth = noBtn.offsetWidth;
+    const buttonHeight = noBtn.offsetHeight;
+
+
+    noBtn.style.transform = `translate(-${buttonX}px, -${buttonY}px)`;
+
+    const randX = Math.floor(Math.random() * (deviceWidth/10));
+    const randY = Math.floor(Math.random() * (deviceHeight/5));
+
+    noBtn.style.transform = `translate(${randX}px,${randY}px)`;
+}
+
+function yes() {
+    const gif = document.getElementById('gif');
+    const text = document.getElementById('text');
+
+    gif.src = './assets/kissing-bear.gif';
+    text.innerHTML = 'I knew you wont say no!';
+
+    yesBtn.style.display = 'none';
+    noBtn.style.display = 'none';
+}
